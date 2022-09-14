a5688847d9d5bedcda4373342c0b71442a1f8853
Miguel Leite <miguelleite360@gmail.com>
2022-09-14 17:10:04 -0300

    done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import registerRouters from "./routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST");
+  app.use(cors());
+  next();
+});
 
 registerRouters(app);
 
