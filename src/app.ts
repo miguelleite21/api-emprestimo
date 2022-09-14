d57de07f8e991be4674d5bfcfc3cfda50d6a3eb8
Miguel Leite <miguelleite360@gmail.com>
2022-09-14 17:56:17 -0300

    finalizado

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
+
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", 'GET,PUT');
     app.use(cors());
     next();
 });
